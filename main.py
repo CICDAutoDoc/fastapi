2aa77e34ad2444a1e9c5445b51bd3377364df409
yjcho <yjcho2013@gmail.com>
2025-10-31 01:18:54 +0900

    modification

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import sys
+import os
+
+# 현재 파일(main.py)이 위치한 디렉토리의 절대 경로를 '동적으로' 알아냅니다.
+PROJECT_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# sys.path 목록에 계산된 경로를 추가합니다.
+if PROJECT_ROOT_DIR not in sys.path:
+    sys.path.append(PROJECT_ROOT_DIR)
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
